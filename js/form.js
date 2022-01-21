87076d61bd0eed352e6fd81f87f0e26481387bbb
FranciscoSule <franciscosule@gmail.com>
2022-01-21 00:12:51 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -8,22 +8,25 @@ document.querySelector("#submit").addEventListener("click", e => {
     let fecha = document.querySelector("#fecha").value;
     let hora = document.querySelector("#hora").value;
     let empleado = document.querySelector("#empleado").value;
+    let servicio = document.querySelector("#servicio").value;
     let resp = document.querySelector("#respuesta");
   
     resp.classList.remove("fail");
     resp.classList.remove("send");
   
-    let url = `https://api.whatsapp.com/send?phone=543425336098&text=
-
-          ${cliente}
-         
-          ${fecha}
-          
-          ${hora}
-          
-          ${empleado}
-          Nos comunicaremos a la brevedad*%0A
-          `;
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+          *_MI NEGOCIO_*%0A
+          *Reservas*%0A%0A
+          *¿Cuál es tu nombre?*%0A
+          ${cliente}%0A
+          *Indica la fecha de tu reserva*%0A
+          ${fecha}%0A
+          *Indica la hora de tu reserva*%0A
+          ${hora}%0A
+          *Empleado de preferencia*%0A
+          ${empleado}%0A
+          *¿Cuál es el servicio que se desea realizar?*%0A
+          ${servicio}`;
   
     if (cliente === "" || fecha === "" || hora === "") {
       resp.classList.add("fail");
