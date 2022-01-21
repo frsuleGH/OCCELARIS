4990654e62bf6c86f03f177de0415254a52d0ed9
FranciscoSule <franciscosule@gmail.com>
2022-01-20 23:50:24 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,9 +1,39 @@
-document.querySelector('#submite').addEventListener('click', function(){
-    let cliente = document.querySelector('#cliente').value;
-    let pedido = document.querySelector('#pedido').value;
-    let pago = document.querySelector('#pago').value;
-
-    let url = "https://api.whatsapp.com/send?phone=543424067600&text=*_Ocelaris3D" + cliente + pedido + pago;
-
+document.querySelector("#submit").addEventListener("click", e => {
+    e.preventDefault();
+  
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "543425336098";
+  
+    let cliente = document.querySelector("#cliente").value;
+    let fecha = document.querySelector("#fecha").value;
+    let hora = document.querySelector("#hora").value;
+    let empleado = document.querySelector("#empleado").value;
+    let servicio = document.querySelector("#servicio").value;
+    let resp = document.querySelector("#respuesta");
+  
+    resp.classList.remove("fail");
+    resp.classList.remove("send");
+  
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+         
+          ${cliente}%0A
+         
+          ${fecha}%0A
+          
+          ${hora}%0A
+          
+          ${empleado}%0A
+          Nos comunicaremos a la brevedad*%0A
+          `;
+  
+    if (cliente === "" || fecha === "" || hora === "") {
+      resp.classList.add("fail");
+      resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+      return false;
+    }
+    resp.classList.remove("fail");
+    resp.classList.add("send");
+    resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+  
     window.open(url);
-})
+  });
