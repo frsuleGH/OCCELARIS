5caf4fc9ef242e886ea95446e21a2effd0ce7ee6
FranciscoSule <franciscosule@gmail.com>
2022-01-25 10:18:06 -0300

    Create scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp(){
+    scrollNav();
+    navegacionFija();
+}
+
+
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.filtro a');
+    enlaces.forEach(enlaces =>{
+        enlaces.addEventListener('click', function(e){
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: 'smooth'});
+        })
+    })
+}
